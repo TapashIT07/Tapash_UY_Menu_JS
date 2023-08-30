f0273195e912c062f59c08ecaa6fcf1d76a826d6
TapashIT07 <tapashit07@gmail.com>
2023-08-30 20:10:21 +0600

    first commit

---
+++ assets/css/app.js
@@ -0,0 +1,34 @@
+// $d(document).ready(function(){
+
+// });
+
+
+$(function(){
+
+$('.menu li:nth-child(3)').click(function(){
+    $('.drop_menu').slideToggle();
+})
+
+$('.collapseButton').click(function(){
+    $('.menu').slideToggle();
+})
+
+
+// $('.btn_1').click(function(){
+
+//     $('.animate_1').animate({opacity:'0.5'},2000);
+// })
+
+// $('.btn_2').click(function(){
+
+//     $('.animate_2').animate({opacity:'0.5'},2000);
+// })
+
+// $('.btn_3').click(function(){
+
+//     $('.animate_3').animate({opacity:'0.5'},500);
+// })
+
+
+
+});
